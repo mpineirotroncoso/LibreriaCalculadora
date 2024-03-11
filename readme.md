ae40a2c1a52aa0b3a5f7c2eab8ed56a05451a29c
Marcos <mpineirotroncoso@danielcastelao.org>
2024-03-11 09:16:30 +0100

    Nueva funcionalidad de raices añadida

---
+++ readme.md
@@ -0,0 +1,12 @@
+1. Crea una rama nueva, que se llame "raices"
+```shell
+git branch raices
+git checkout raices
+```
+2. Añade una nueva operación, que realize la raíz cuadrada o la raiz cúbica. El primer numero que mandamos es el radicando y el segundo el índice.
+3. Realiza varios commits según vayas avanzando en la programación
+4. Mezcla con squash a la rama principal
+5. Crea una nueva release y sube el nuevo jar con la nueva operación para que se pueda usar como una libreria.
+6. ¿Podrías hacer que calculara para cualquier tipo de índice, no solo raices cuadradas y cúbicas?
+
+Si, Línea 120 de Calcular.java
--- src/com/marcos/libreria/Calcular.java
+++ src/com/marcos/libreria/Calcular.java
@@ -21,6 +21,10 @@ public class Calcular {
      * Opcion de division
      */
     public static final String DIVISION = "division";
+    /**
+     * Opcion de raices
+     */
+    public static final String RAIZ = "raiz";
 
     /**
      * @param num1      primer valor
@@ -43,6 +47,9 @@ public class Calcular {
                 break;
             case DIVISION:
                 resultado = division(num1, num2);
+                break;
+            case RAIZ:
+                resultado = raiz(num1, num2);
                 break;
             default:
                 System.out.println("parametro no valido: " + operacion);
@@ -99,6 +106,24 @@ public class Calcular {
     private static Float division(float num1, float num2) {
         try {
             return num1 / num2;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param radicando de la raiz
+     * @param indice de la raiz
+     * @return resultado
+     */
+    private static Float raiz(float radicando, float indice) {
+        // Descomentar para solo permitir raices cuadradas y cubicas
+        /*if (indice != 2 && indice != 3) {
+            System.out.println("El indice debe ser 2 o 3");
+            return null;
+        }*/
+        try {
+            return (float) Math.pow(radicando, 1/indice);
         } catch (Exception e) {
             return null;
         }
